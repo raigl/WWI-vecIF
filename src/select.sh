be61c03a7806524db9f5f334ff758c1ca076b2f2
Rainer Glaschick <rainer@glaschick.de>
2023-09-27 07:00:16 +0200

    added TWS to select.sh

--- src/select.sh
+++ src/select.sh
@@ -14,9 +14,11 @@ do $base/select.py
       4) p="wwsim $code/Bounce/r-196-bounce-example/bounce.acore";;
       5) p="wwsim $code/Mad-Game/mad-game.acore";;
       6) p="wwsim $code/Blackjack/bjack.acore";;
+      7) p="wwsim $code/Diags/crt-test-68_001_fbl00-0-50.tcore";;
       8) p="wwsim $code/Vibrating-String/fb131-97-setup-and-run-Annotated.acore";;
       9) p="wwsim $code/Vibrating-String/fc131-204-2-merged-annotated.acore";;
      10) p="wwsim $code/Vibrating-String/fc131-204-6-merged-annotated.acore";;
+     11) p="wwsim $code/Track-While-Scan-D-Israel/annotated-track-while-scan.acore -D -r --CrtF 5 --NoToggl --Ana -c 0 -q";;
      15) exit;;
     127) exit;;
       *) continue;;
@@ -26,4 +28,3 @@ do $base/select.py
 done
                         
                         
- 
