8e4bc5e9546ffc215f6bada6052c5bffabf4b98a
Rainer Glaschick <rainer@glaschick.de>
2023-10-11 21:28:04 +0200

    Fixes in Schematics

---
+++ select.sh
@@ -0,0 +1,32 @@
+#
+code=~pi/wwi/Whirlwind-Instruction-Simulator/Code-Samples
+base=~pi/wwi/WWI-VecIF/src
+while true
+do $base/select.py
+   rc=$?
+   echo rc=$rc
+   pwd
+   p="continue"
+   case $rc in
+      0) continue;;
+      1) p="$base/vecIF.py";;
+      2) p="$base/tictactoe.py";;
+      3) continue;;
+      4) p="wwsim $code/Bounce/r-196-bounce-example/bounce.acore";;
+      5) p="wwsim $code/Mad-Game/mad-game.acore";;
+      6) p="wwsim $code/Blackjack/bjack.acore";;
+      7) p="wwsim $code/Diags/crt-test-68_001_fbl00-0-50.tcore";;
+      8) p="wwsim $code/Vibrating-String/fb131-97-setup-and-run-Annotated.acore";;
+      9) p="wwsim $code/Vibrating-String/fc131-204-2-merged-annotated.acore";;
+     10) p="wwsim $code/Vibrating-String/fc131-204-6-merged-annotated.acore";;
+     11) cd $code/Track-While-Scan-D-Israel/; p="wwsim annotated-track-while-scan.acore -D -r --CrtF 5 --NoToggl --Ana -c 0 -q";;
+     12) p="wwsim $code/Number-Display/number-display-annotated";;
+     15) exit;;
+    127) exit;;
+      *) continue;;
+   esac
+   echo "prog=$p"
+   $p
+done
+                        
+                        
